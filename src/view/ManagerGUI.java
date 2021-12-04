715faff73b963dbe63082fc0e2c98de2449b2061
DaveSharma-Hub <81478885+DaveSharma-Hub@users.noreply.github.com>
2021-12-04 09:36:28 -0700

    Update ManagerGUI.java

--- src/view/ManagerGUI.java
+++ src/view/ManagerGUI.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
@@ -8,13 +9,14 @@ import java.awt.*;
 public class ManagerGUI extends JFrame implements ActionListener, MouseListener {
 
     private JLabel generalMessage1;
-
+    
     private JButton editPropertyButton;
     private JButton editLandlordButton;
     private JButton editRenterButton;
     private JButton viewPropertyButton;
     private JButton viewLandlordButton;
     private JButton viewRenterButton;
+    private JButton backButton;
 
 
     public ManagerGUI() {
@@ -27,40 +29,51 @@ public class ManagerGUI extends JFrame implements ActionListener, MouseListener 
      * Sets gui.
      */
     public void setupGUI() {
-
-        String options[]= {"Edit","View"};
-        //Let's set up the JLabels and the JTextFields and the JButton for our GUI.
+        
+    	String options[]= {"Edit","View"};
+    	//Let's set up the JLabels and the JTextFields and the JButton for our GUI.
         generalMessage1 = new JLabel("Manager System");
+
+//        usernameTextField = new JTextField("User's username", 18);
+//        passwordTextField = new JTextField("User's password", 18);
 
         editPropertyButton = new JButton("Edit Property");
         editLandlordButton = new JButton("Edit Landlord");
         editRenterButton = new JButton("Edit Renter");
-
+        
         viewPropertyButton = new JButton("View Property");
         viewLandlordButton = new JButton("View Landlord");
         viewRenterButton = new JButton("View Renter");
+        
+        backButton = new JButton("Back");
+
         //add Mouse Listeners to the JTextFields and ActionListener to the JButton
 
         editPropertyButton.addActionListener(this);
         editLandlordButton.addActionListener(this);
         editRenterButton.addActionListener(this);
-
+        
         viewPropertyButton.addActionListener(this);
         viewLandlordButton.addActionListener(this);
         viewRenterButton.addActionListener(this);
+        
+        backButton.addActionListener(e -> {
+            super.dispose();
+        });
         //Create the JPanels.
         JPanel mainContainer = new JPanel();
         JPanel headerPanel = new JPanel();
         JPanel editPanel = new JPanel();
         JPanel viewPanel = new JPanel();
-
+        JPanel backPanel = new JPanel();
 
         //Set the Layouts for the JPanels
         mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.PAGE_AXIS));
         headerPanel.setLayout(new FlowLayout());
-
+        
         editPanel.setLayout(new FlowLayout());
         viewPanel.setLayout(new FlowLayout());
+        backPanel.setLayout(new FlowLayout());
         //Add Components to the JPanels.
 
         headerPanel.add(generalMessage1);
@@ -68,16 +81,17 @@ public class ManagerGUI extends JFrame implements ActionListener, MouseListener 
         editPanel.add(editRenterButton);
         editPanel.add(editLandlordButton);
         editPanel.add(editPropertyButton);
-
+        
         viewPanel.add(viewRenterButton);
         viewPanel.add(viewLandlordButton);
         viewPanel.add(viewPropertyButton);
-
+        
+        backPanel.add(backButton);
         //Add the JPanels to the main JPanel
         mainContainer.add(headerPanel);
         mainContainer.add(editPanel);
         mainContainer.add(viewPanel);
-
+        mainContainer.add(backPanel);
         //Add the main panel to the JFrame.
         this.add(mainContainer);
     }
@@ -89,11 +103,20 @@ public class ManagerGUI extends JFrame implements ActionListener, MouseListener 
      */
     public void actionPerformed(ActionEvent e) {
         //Pull the data from the JTextFields username, password and url
+//        username = usernameTextField.getText();
+//        password = passwordTextField.getText();
         //Attempt to create a databaseAccess object called database using the inputs provided by the user.
     }
 
     public void mouseClicked(MouseEvent event) {
 
+//        if(event.getSource().equals(usernameTextField)) {
+//            usernameTextField.setText("");
+//        }
+//
+//        if(event.getSource().equals(passwordTextField)) {
+//            passwordTextField.setText("");
+//        }
     }
 
     public void mouseExited(MouseEvent event) {
