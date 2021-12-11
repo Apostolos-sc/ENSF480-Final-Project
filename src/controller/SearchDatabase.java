67ca329bcec60dc42a9c49f964a8d09e25a7675f
Dave Sharma <81478885+DaveSharma-Hub@users.noreply.github.com>
2021-12-10 17:59:14 -0700

    Update SearchDatabase.java

--- src/controller/SearchDatabase.java
+++ src/controller/SearchDatabase.java
@@ -106,23 +106,24 @@ public ArrayList<Property> managerReport(int days){
         }
     }
 
-    public PeriodicalReport createReport(String startOfPer, String endOfPer){
-        Date startDate = java.sql.Date.valueOf(startOfPer);
-        Date endDate = java.sql.Date.valueOf(endOfPer);
+        public PeriodicalReport createReport(String startOfPer, String endOfPer){
+//        Date startDate = java.sql.Date.valueOf(startOfPer);
+//        Date endDate = java.sql.Date.valueOf(endOfPer);
+
         ArrayList<Property> listedInPeriod = new ArrayList<>();
         ArrayList<Property> rentedInPeriod = new ArrayList<>();
         int numberOfHousesRented = 0;
         int totalActiveListings = 0;
         try (Statement stmt = dbConnect.createStatement()) {
-            ResultSet results = stmt.executeQuery("SELECT * FROM PROPERTY WHERE startDate >=" + startOfPer + "startDate < "+endOfPer);
-            while (results.next()) {// takes into account number of rows that were returned by the query
+            ResultSet results = stmt.executeQuery("SELECT * FROM PROPERTY");
+        	while (results.next()) {// takes into account number of rows that were returned by the query
                 ResultSetMetaData rsmd = results.getMetaData();
-                if (results.getString("state").equals("Listed")) {
+                if (results.getString("state").equals("Listed") && (results.getInt("startDate")>=Integer.valueOf(startOfPer) && results.getInt("endDate")<=Integer.valueOf(endOfPer))) {
                     Property prop= new Property(Integer.valueOf(results.getString("propertyID")), results.getString("propertyType"), Integer.valueOf(results.getString("noBathrooms")),
                         Integer.valueOf(results.getString("noBedrooms")), results.getBoolean("isFurnished"), results.getString("address"),
                         results.getString("quadrant"), results.getString("state"), results.getInt("Price"));
                     listedInPeriod.add(prop);
-                }else if(results.getString("state").equals("Rented")){
+                }else if(results.getString("state").equals("Rented")&& (results.getInt("startDate")>=Integer.valueOf(startOfPer) && results.getInt("endDate")<=Integer.valueOf(endOfPer))){
                     Property prop = new Property(Integer.valueOf(results.getString("propertyID")),
                             results.getString("propertyType"), Integer.valueOf(results.getString("noBathrooms")),
                             Integer.valueOf(results.getString("noBedrooms")), results.getBoolean("isFurnished"),
@@ -139,10 +140,12 @@ public ArrayList<Property> managerReport(int days){
         }
         try (Statement stmt = dbConnect.createStatement()) {
             ResultSet results = stmt
-                    .executeQuery("SELECT * FROM PROPERTY WHERE state = 'Listed'");
+                    .executeQuery("SELECT * FROM PROPERTY");
             while (results.next()) {// takes into account number of rows that were returned by the query
                 ResultSetMetaData rsmd = results.getMetaData();
-                    totalActiveListings++;
+                if(results.getString("state").equals("Listed")) {    
+                	totalActiveListings++;
+                }
             }
             stmt.close();
             results.close();
