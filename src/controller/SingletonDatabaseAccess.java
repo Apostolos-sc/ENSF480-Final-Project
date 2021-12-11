8e6188237ccb68cc8ab2d4eeb09bcf5958c5fbda
Nuha <nuha.shaikh@gmail.com>
2021-12-10 20:35:27 -0700

    baseline documentation

--- src/controller/SingletonDatabaseAccess.java
+++ src/controller/SingletonDatabaseAccess.java
@@ -1,12 +1,13 @@
+/** 
+@author Dave Sharma <a href="mailto:dave.sharma@ucalgary.ca">agampreet.aulakh@ucalgary.ca </a>
+Nuha Shaikh <a href="mailto:nuha.shaikh1@ucalgary.ca">nuha.shaikh1@ucalgary.ca</a>
+Apostolos Scondrianis <a href="mailto:apostolos.scondriani@ucalgary.ca">huda.abbas@ucalgary.ca</a>
+Harsh Sharma <a href= "mailto:harshit.sharma@ucalgary.ca">melanie.nguyen@ucalgary.ca</a>
+@version 2.2
+@since  2.0
+  * Data.java (insert explanation of class)
+*/
 package controller;
-
-/**
- @version 1.0
- @since  1.0
-  * DatabaseAccess.java accesses and updates the SQL inventory database. It has data members DBURL to specify
-  * which database is being accessed, USERNAME to specific username of user, PASSWORD and dbConnect
-  * to establish a connection to the database.
- */
 
 import model.*;
 import java.sql.*;
--- src/model/PropertyLocation.java
+++ src/model/PropertyLocation.java
@@ -1,3 +1,12 @@
+/** 
+@author Dave Sharma <a href="mailto:dave.sharma@ucalgary.ca">agampreet.aulakh@ucalgary.ca </a>
+Nuha Shaikh <a href="mailto:nuha.shaikh1@ucalgary.ca">nuha.shaikh1@ucalgary.ca</a>
+Apostolos Scondrianis <a href="mailto:apostolos.scondriani@ucalgary.ca">huda.abbas@ucalgary.ca</a>
+Harsh Sharma <a href= "mailto:harshit.sharma@ucalgary.ca">melanie.nguyen@ucalgary.ca</a>
+@version 2.2
+@since  2.0
+  * Data.java (insert explanation of class)
+*/
 package model;
 
 public class PropertyLocation {
